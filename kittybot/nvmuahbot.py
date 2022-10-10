ecdc7d3cb00b768b6f7a46da98c12ce00392fa86
Всеволод Воробьев <amaterasuowned@hotmail.com>
2022-10-10 19:17:58 +0700

    1

---
+++ kittybot/nvmuahbot.py
@@ -0,0 +1,44 @@
+from telegram import Bot, ReplyKeyboardMarkup
+from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+
+
+updater = Updater(token="5334912797:AAFpYMe5Po23KD8_k7s_PEpUtMH0E3w_bUU")
+
+
+def say_hi(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(
+        chat_id=chat.id,
+        text="Привет, я бот Натальи @NVMuah <- нажми  для связи с мастером !",
+    )
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    buttons = ReplyKeyboardMarkup(
+        [
+            ["Записаться на образ(макияж + укладка)", "Записаться на макияж"],
+            [
+                "Записаться на укладку",
+                "Портфолио работ IG",
+                "Портфолио работ VK",
+                "Написать мастеру",
+            ],
+        ]
+    )
+    context.bot.send_message(
+        chat_id=chat.id,
+        text="Спасибо, что включили меня,{}!".format(name),
+        reply_markup=buttons,
+    )
+
+
+bot = Bot(token="5334912797:AAFpYMe5Po23KD8_k7s_PEpUtMH0E3w_bUU")
+
+updater.dispatcher.add_handler(CommandHandler("start", wake_up))
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+updater.start_polling(poll_interval=15.0)
+updater.idle()
